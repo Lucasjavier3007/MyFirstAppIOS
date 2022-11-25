a9694ea1c50b54883a2035ab0c93902bb71f2c0d
Lucasjavier3007 <lucasjavoeroviedo@gmail.com>
2022-11-24 21:23:34 -0300

    Initial Commit

---
+++ MyFirstAppIOS/MyFirstAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppIOSApp.swift
+//  MyFirstAppIOS
+//
+//  Created by Lucas javier Oviedo on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
